a1bcd830014993783e762913f0cbbf39464ef260
Samuel Carlsson <samuel.carlsson@gmail.com>
2013-07-10 16:24:26 +0200

    Refactor: Moving out DownloadString to extension method

---
+++ WP8/EulerProfile/Common/SweetWebClientExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace EulerProfile.Common
+{
+    static class SweetWebClientExtensions
+    {
+        public static Task<string> DownloadString(this WebClient wc, Uri url)
+        {
+            var tcs = new TaskCompletionSource<string>();            
+            wc.DownloadStringCompleted += (s, e) =>
+            {
+                if (e.Error != null) tcs.TrySetException(e.Error);
+                else if (e.Cancelled) tcs.TrySetCanceled();
+                else tcs.TrySetResult(e.Result);
+            };
+            wc.DownloadStringAsync(url);
+            return tcs.Task;
+        }
+    }
+}
--- WP8/EulerProfile/Repositories/ProjectEulerRepository.cs
+++ WP8/EulerProfile/Repositories/ProjectEulerRepository.cs
@@ -2,29 +2,17 @@
 using EulerProfile.Model;
 using System.Net;
 using System.Threading.Tasks;
+using EulerProfile.Common;
 
 namespace EulerProfile.Repositories
 {
     class ProjectEulerRepository
     {
-        public static Task<string> DownloadString(Uri url)
-        {
-            var tcs = new TaskCompletionSource<string>();
-            var wc = new WebClient();
-            wc.DownloadStringCompleted += (s, e) =>
-            {
-                if (e.Error != null) tcs.TrySetException(e.Error);
-                else if (e.Cancelled) tcs.TrySetCanceled();
-                else tcs.TrySetResult(e.Result);
-            };
-            wc.DownloadStringAsync(url);
-            return tcs.Task;
-        }
-
         public async Task<UserProfile> GetByUsername(string username)
         {
             Uri uri = new Uri("http://projecteuler.net/profile/" + username + ".txt");
-            string rawProfile = await DownloadString(uri);
+            var webClient = new WebClient();
+            string rawProfile = await webClient.DownloadString(uri);
             var parts = rawProfile.Split(',');
             if (parts.Length < 5) throw new ApplicationException(string.Format("User {0} not found", username));
             return new UserProfile(parts[0], parts[1], parts[2], parts[3], parts[4]);
