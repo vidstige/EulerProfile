7901ffc8c263d6383a768f48b26a98b650d463b7
Samuel Carlsson <samuel.carlsson@gmail.com>
2013-07-10 12:14:27 +0200

    Adding repo for fetching profiles

---
+++ WP8/EulerProfile/Model/UserProfile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EulerProfile.Model
+{
+    class UserProfile
+    {
+        private string _username;
+        private string _country;
+        private string _unknown;
+        private string _solved;
+        private string _level;
+
+        public UserProfile(string username, string country, string unknown, string solved, string level)
+        {
+            _username = username;
+            _country = country;
+            _unknown = unknown;
+            _solved = solved;
+            _level = level;
+        }
+
+        public Uri LevelIcon
+        {
+            get { return new Uri("http://projecteuler.net/images/levels/level_1.png"); }
+        }
+    }
+}
